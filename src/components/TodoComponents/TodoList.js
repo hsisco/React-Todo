a501625cb5cb8eefa33397dda12c55d3efa94690
hsisco <hysensisco@gmail.com>
2020-01-15 18:42:11 -0800

    Todo into TodoList

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Todo from './Todo';
 
 const TodoList = props => {
   return (
     <div className="hex-list">
-      
+      {props.hexes.map(todo => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          toggleCast={props.toggleCast}
+          />
+      ))}
     </div>
   );
 };
