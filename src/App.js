6799b5564d329f8249f292871782b1e4069c75a7
hsisco <hysensisco@gmail.com>
2020-01-15 18:34:28 -0800

    App state

--- src/App.js
+++ src/App.js
@@ -19,9 +19,10 @@ const data = [
 ];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    hexes: data
+  };
+  
   render() {
     console.log(this.state.hexes);
     return (
