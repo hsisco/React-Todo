74ccd482dc83c6df60ce0532a1bc6ef5db521707
hsisco <hysensisco@gmail.com>
2020-01-15 18:31:44 -0800

    Initial upload, seed data created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const data = [
+  {
+    hex: 'Smudge House',
+    id: 1528817077286,
+    cast: false
+  },
+  {
+    hex: 'Invoke Gma Ursula for pie recipe',
+    id: 1579068659537,
+    cast: false,
+  },
+  {
+    hex: 'Hex the patriarchy',
+    id: 1528817084358,
+    cast: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
